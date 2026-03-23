58301f90b406e8102943f681180065a0317944de
Leonardo Santos <contato.leodsantos@gmail.com>
2026-03-23 11:20:54 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,3 @@
+x = 10
+y = 5
+print(not y == 5 and x > 10 or x < 20)
---
+++ script.py
@@ -0,0 +1,6 @@
+idade =int(input("Informe sua idade: "))
+if idade >= 16 and idade <=69:
+    resultado = "sim"
+else:
+    resultado = "não"
+print(f"Pode doar sangue: {resultado}")
